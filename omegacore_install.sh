6a5ec954bc9315a8a829617b75328a660b1bc3c4
Zoldur <zoldur99@gmail.com>
2018-06-16 03:19:30 -0700

    Get IP

--- omegacore_install.sh
+++ omegacore_install.sh
@@ -8,7 +8,7 @@ OMEGA_REPO="https://github.com/omegacoinnetwork/omegacoin/releases/download/0.12
 SENTINEL_REPO="https://github.com/omegacoinnetwork/sentinel.git"
 DEFAULTOMEGAPORT=7777
 DEFAULTOMEGAUSER="omega"
-NODEIP=$(curl -s4 icanhazip.com)
+NODEIP=$(curl -s4 api.ipify.org)
 
 
 RED='\033[0;31m'
@@ -20,7 +20,7 @@ function get_ip() {
   declare -a NODE_IPS
   for ips in $(netstat -i | awk '!/Kernel|Iface|lo/ {print $1," "}')
   do
-    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 icanhazip.com))
+    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 api.ipify.org))
   done
 
   if [ ${#NODE_IPS[@]} -gt 1 ]
